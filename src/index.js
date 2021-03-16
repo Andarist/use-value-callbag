f8671afc5a195c5baf2763c79f1e6ba96b1ec2af
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2021-03-16 16:57:21 +0100

    fix layout effect redundant emissions, fix fast-refresh subject termination

--- src/index.js
+++ src/index.js
@@ -3,16 +3,29 @@ import { useRef, useEffect } from 'react'
 
 export default function useValueCallbag(value) {
   const value$Ref = useRef()
+  const lastVal = useRef(value)
 
   if (!value$Ref.current) {
     value$Ref.current = behaviorSubject(value)
+    lastVal.current = value
   }
 
+  const subject = value$Ref.current
+
   useEffect(() => {
-    value$Ref.current(1, value)
-  }, [value])
+    if (lastVal.current !== value) {
+      subject(1, value)
+      lastVal.current = value
+    }
+  }, [value, subject])
 
-  useEffect(() => () => value$Ref.current(2), [])
+  useEffect(
+    () => () => {
+      value$Ref.current(2)
+      value$Ref.current = undefined
+    },
+    [],
+  )
 
-  return value$Ref.current
+  return subject
 }
